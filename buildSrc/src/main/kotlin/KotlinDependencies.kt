a518715d8f168dc02a75a484c260d1bbc897a73e
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-07 15:04:32 +0200

    Start migrating to gradle kotlin dsl

---
+++ buildSrc/src/main/kotlin/KotlinDependencies.kt
@@ -0,0 +1,16 @@
+object BuildConfigVersions {
+    val compileSdkVersion = 30
+    val minSdkVersion = 16
+    val targetSdkVersion = 30
+    val versionCode = 1
+    val versionName = "0.0.1"
+}
+
+object Versions {
+    val kotlin = "1.5.0"
+}
+
+
+object KotlinDependencies {
+    val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
