b283511da83af3656353cf663054581105c2b123
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-11 13:47:05 +0200

    Added note domain model

---
+++ app/src/main/java/com/mg/cleanarchitectureplaygraoun/business/domain/model/Note.kt
@@ -0,0 +1,13 @@
+package com.mg.cleanarchitectureplaygraoun.business.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Note(
+    val id: String,
+    val title: String,
+    val body: String,
+    val createdAt: String,
+    val updatedAt: String
+) : Parcelable
