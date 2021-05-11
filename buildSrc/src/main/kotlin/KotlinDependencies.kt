d8ac18050ac0cdcf78b55300cea9d2a26b476d63
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-11 13:37:06 +0200

    Dependencies cleanup

--- buildSrc/src/main/kotlin/KotlinDependencies.kt
+++ buildSrc/src/main/kotlin/KotlinDependencies.kt
@@ -7,10 +7,28 @@ object BuildConfigVersions {
 }
 
 object Versions {
+    val gradle = "4.2.0"
     val kotlin = "1.5.0"
+    val coreKtx = "1.3.2"
+    val appCompat = "1.2.0"
+    val materialComponents = "1.3.0"
+    val constraintlayout = "2.0.4"
+    val kotest = "4.4.3"
 }
-
 
 object KotlinDependencies {
     val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
+
+object AndroidDependencies {
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.materialComponents}"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
+}
+
+object UnitTestsDependencies {
+    val kotestRunner = "io.kotest:kotest-runner-junit5-jvm:${Versions.kotest}"
+    val kotestFrameworkEngine = "io.kotest:kotest-framework-engine-jvm:${Versions.kotest}"
+    val kotestCore = "io.kotest:kotest-assertions-core:${Versions.kotest}"
 }
