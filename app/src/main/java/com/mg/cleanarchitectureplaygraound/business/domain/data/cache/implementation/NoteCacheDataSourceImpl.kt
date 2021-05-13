24ac128608f6441f8b4bd35d8640a18bec6e30bb
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-13 12:58:29 +0200

    Added empty note cache data source implementation

---
+++ app/src/main/java/com/mg/cleanarchitectureplaygraound/business/domain/data/cache/implementation/NoteCacheDataSourceImpl.kt
@@ -0,0 +1,36 @@
+package com.mg.cleanarchitectureplaygraound.business.domain.data.cache.implementation
+
+import com.mg.cleanarchitectureplaygraound.business.domain.data.cache.abstraction.NoteCacheDataSource
+import com.mg.cleanarchitectureplaygraound.business.domain.data.model.Note
+import com.mg.cleanarchitectureplaygraound.business.domain.types.Id
+
+class NoteCacheDataSourceImpl(
+    private val noteDaoService: NoteDaoService
+) : NoteCacheDataSource {
+
+    override suspend fun insertNote(note: Note): Id = noteDaoService.insertNote(note)
+
+    override suspend fun deleteNote(primary: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNotes(note: List<Note>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateNote(primary: String, newTitle: String, newBody: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchNotes(query: String, filterAndOrder: String, page: Int): List<Note> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchById(id: Id): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNumberOfNotes(): Int {
+        TODO("Not yet implemented")
+    }
+}
