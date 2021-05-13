09be2d2d89e59745d73ce9ac429e580d30f7b0a3
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-13 13:12:45 +0200

    Added network data source implementation

---
+++ app/src/main/java/com/mg/cleanarchitectureplaygraound/business/domain/data/network/implementation/NetworkDataSourceImpl.kt
@@ -0,0 +1,50 @@
+package com.mg.cleanarchitectureplaygraound.business.domain.data.network.implementation
+
+import com.mg.cleanarchitectureplaygraound.business.domain.data.model.Note
+import com.mg.cleanarchitectureplaygraound.business.domain.data.network.abstraction.NetworkDataSource
+import com.mg.cleanarchitectureplaygraound.business.domain.types.Id
+
+class NetworkDataSourceImpl(
+    private val firestoreService: NoteFirestoreService
+) : NetworkDataSource {
+
+    override suspend fun insertOrUpdateNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(id: Id) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertDeletedNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertDeletedNotes(notes: List<Note>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteDeteledNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getDeletedNotes(): List<Note> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllNotes() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchNote(note: Note): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllNotes(): List<Note> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOrUpdateNotes(notes: List<Note>) {
+        TODO("Not yet implemented")
+    }
+}
