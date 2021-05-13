faae2bb824f309a5676764f6e89fe15eae8b7b91
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-13 13:25:27 +0200

    Added some network errors

---
+++ app/src/main/java/com/mg/cleanarchitectureplaygraound/business/domain/data/network/NetworkErrors.kt
@@ -0,0 +1,7 @@
+package com.mg.cleanarchitectureplaygraound.business.domain.data.network
+
+const val NETWORK_ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
+const val NETWORK_ERROR_UNKNOWN = "Unknown network error"
+const val NETWORK_ERROR = "Network error"
+const val NETWORK_ERROR_TIMEOUT = "Network timeout"
+const val NETWORK_DATA_NULL = "Network data is null"
