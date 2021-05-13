a70a64d565818448cd29b99129eddc8573be5a3b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-13 13:03:15 +0200

    Added cache errors

---
+++ app/src/main/java/com/mg/cleanarchitectureplaygraound/business/domain/data/cache/CacheErrors.kt
@@ -0,0 +1,6 @@
+package com.mg.cleanarchitectureplaygraound.business.domain.data.cache
+
+const val CACHE_ERROR_UNKNOWN = "Unknown cache error"
+const val CACHE_ERROR = "Cache error"
+const val CACHE_ERROR_TIMEOUT = "Cache timeout"
+const val CACHE_ERROR_DATA_NULL = "Cache data is null"
