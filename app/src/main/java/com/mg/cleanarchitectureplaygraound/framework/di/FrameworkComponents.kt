56151d554eaadcdf1ac63a5abb4f515bfa67a165
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-25 13:19:25 +0200

    Added framework components

--- app/src/main/java/com/mg/cleanarchitectureplaygraound/framework/di/FrameworkComponents.kt
+++ app/src/main/java/com/mg/cleanarchitectureplaygraound/framework/di/FrameworkComponents.kt
@@ -1,7 +1,12 @@
 package com.mg.cleanarchitectureplaygraound.framework.di
 
+import com.mg.cleanarchitectureplaygraound.framework.datasource.cache.service.NoteDaoService
+import com.mg.cleanarchitectureplaygraound.framework.datasource.cache.service.impl.DatabaseNoteDaoService
+import com.mg.cleanarchitectureplaygraound.framework.datasource.network.service.NoteWebserviceService
+import com.mg.cleanarchitectureplaygraound.framework.datasource.network.service.impl.NoteFirestoreServiceImpl
 import org.koin.dsl.module
 
 val frameworkModule = module {
-
+    factory<NoteDaoService> { DatabaseNoteDaoService() }
+    factory<NoteWebserviceService> { NoteFirestoreServiceImpl() }
 }
